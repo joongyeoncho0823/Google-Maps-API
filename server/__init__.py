b547343173b20601ce225157d8ae35b62335b1d6
Steven <joongyeoncho0823@gmail.com>
2022-04-08 16:18:22 -0400

    pipreqs, delete test.py

--- server/__init__.py
+++ server/__init__.py
@@ -6,9 +6,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    from .test import test
     from .views import views
-    app.register_blueprint(test, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
 
     return app
